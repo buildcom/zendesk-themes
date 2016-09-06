d44d45a3f6cd864eb2da985805ff1bafc015a8f7
Seth Davis <setholito@gmail.com>
2016-09-06 15:53:03 -0500

    No more minify css.

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,27 @@ var gulp = require('gulp'),
     sass = require('gulp-sass')
     cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', ['sass','watch']);
+var paths = {
+  es: {
+    scss: './edgestar/style/scss/',
+    css: './edgestar/style/css/',
+  }
+};
+
+gulp.task('default', ['sass', 'watch']);
 
 gulp.task('sass', function(){
-  return gulp.src('./edgestar/style/scss/*.scss')
+  return gulp.src(paths.es.scss + '*.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./edgestar/style/css'));
+  .pipe(gulp.dest(paths.es.css));
 });
 
-gulp.task('compile-minify-css', ['sass'], function() {
-  return gulp.src('./edgestar/style/css/*.css')
+gulp.task('compile-minify-css', function() {
+  return gulp.src(paths.es.css + '*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./edgestar/style/css/'));
+    .pipe(gulp.dest(paths.es.css));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./edgestar/style/scss/*.scss', ['compile-minify-css']);
+  gulp.watch(paths.es.scss + '*.scss', ['sass']);
 });
-
